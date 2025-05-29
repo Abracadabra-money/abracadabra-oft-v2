479b14ef633496a5a3d3f0268a1873df406d0a17
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2025-05-29 11:59:10 -0400

    deployement

--- deploy/SpellOFT.ts
+++ deploy/SpellOFT.ts
@@ -1,5 +1,5 @@
 import { Contract, ethers } from 'ethers'
-import { type DeployFunction } from 'hardhat-deploy/types'
+import { DeployOptions, type DeployFunction } from 'hardhat-deploy/types'
 import { getDeploymentAddressAndAbi } from '@layerzerolabs/lz-evm-sdk-v2'
 
 const deploymentName = 'SpellOFT'
@@ -10,19 +10,29 @@ const configurations = {
         contractName: 'AbraOFTAdapterUpgradeable',
         args: (endpointAddress: string) => ['0x090185f2135308BaD17527004364eBcC2D37e5F6', endpointAddress], // SPELL address
         initializeArgs: (signer: string) => [signer],
-        feeHandler: '0xe4aec83Cba57E2B0b9ED8bc9801123F44f393037'
+        feeHandler: '0xe4aec83Cba57E2B0b9ED8bc9801123F44f393037',
+        useDeterministicDeployment: true
     },
     'arbitrum-mainnet': {
         contractName: 'AbraOFTUpgradeable',
         args: (endpointAddress: string) => [endpointAddress],
         initializeArgs: (signer: string) => ['SPELL', 'SPELL', signer],
-        feeHandler: '0xE66BE95FE4E3889a66925d996AF3E4dC173754a2'
+        feeHandler: '0xE66BE95FE4E3889a66925d996AF3E4dC173754a2',
+        useDeterministicDeployment: true
     },
     'bera-mainnet': {
         contractName: 'AbraOFTUpgradeable',
         args: (endpointAddress: string) => [endpointAddress],
         initializeArgs: (signer: string) => ['SPELL', 'SPELL', signer],
-        feeHandler: ethers.constants.AddressZero
+        feeHandler: ethers.constants.AddressZero,
+        useDeterministicDeployment: true
+    },
+    'nibiru-mainnet': {
+        contractName: 'AbraOFTUpgradeable',
+        args: (endpointAddress: string) => [endpointAddress],
+        initializeArgs: (signer: string) => ['SPELL', 'SPELL', signer],
+        feeHandler: "0x279D54aDD72935d845074675De0dbcfdc66800a3",
+        useDeterministicDeployment: true
     }
 }
 
@@ -32,12 +42,10 @@ const deploy: DeployFunction = async (hre) => {
     console.log(`deploying ${deploymentName} on network: ${hre.network.name} with ${signer.address}`)
 
     const { address, abi } = getDeploymentAddressAndAbi(hre.network.name, 'EndpointV2')
-    const endpointV2Deployment = new Contract(address, abi, signer)
-
+    const endpointV2Deployment = await hre.ethers.getContractAt(abi, address)
     const config = configurations[hre.network.name as keyof typeof configurations]
 
-    const deployment = await deploy(deploymentName, {
-        deterministicDeployment: "0x" + Buffer.from(salt).toString('hex'),
+    const deployOptions = {
         from: signer.address,
         args: config.args(endpointV2Deployment.address),
         log: true,
@@ -54,7 +62,16 @@ const deploy: DeployFunction = async (hre) => {
             },
         },
         contract: config.contractName
-    })
+    } as DeployOptions;
+    
+    if (config.useDeterministicDeployment) {
+        console.log(`using deterministic deployment: ${salt}`)
+        deployOptions.deterministicDeployment = "0x" + Buffer.from(salt).toString('hex');
+    } else {
+        console.log(`using non-deterministic deployment`)
+    }
+    
+    const deployment = await deploy(deploymentName, deployOptions);
 
     if (config.feeHandler !== ethers.constants.AddressZero) {
         const oft = await hre.ethers.getContractAt('SenderWithFees', deployment.address)
